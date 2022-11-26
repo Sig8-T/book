614218a63c36e639b0c7a6d44a4fd088bfb41f3a
Sig8-T <astronomy.start0815@gmail.com>
2022-11-26 15:30:43 +0900

    push_book

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-	serviceDomain: "sampleblog0824",
+	serviceDomain: "k9bod1y5sm",
 	apiKey: process.env.API_KEY,
 });
